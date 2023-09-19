eac4715ecdfd4cc09cbd598308f1eca4a01e71cf
Cristian <cristianbrogni_28@hotmail.com>
2023-09-19 15:24:14 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo Homem De Ferro',
+    description: 'marvel anuncia um novo filme do homem de ferro, confira',
+    photoCover:
+      'https://wallpapers.com/images/hd/iron-man-pictures-k83lqhe6vnh95b1m.jpg',
+  },
+  {
+    id: '2',
+    title: 'Nova serie anunciada no Disney +',
+    description: 'bla bla bla',
+    photoCover:
+      'https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2022/10/legiao_D8mFjTNqpZY5.png.webp',
+  },
+];
